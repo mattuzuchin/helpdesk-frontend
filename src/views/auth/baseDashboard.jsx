024962c37aecf023c5a11761fb0290a24b5ae650
mattuzuchin <mu26@uw.edu>
2026-05-26 15:46:19 -0700

    new logo

--- src/views/auth/baseDashboard.jsx
+++ src/views/auth/baseDashboard.jsx
@@ -210,7 +210,7 @@ export function BaseDashboard({ name, tickets, menuOpen, setMenuOpen, success, e
           <img
             src="/hds.png"
             alt="logo"
-            style={{ width: 80, height: 54, objectFit: "contain" }}
+            style={{ maxHeight: 60, width: "auto", display: "block" }}
           />
         </Button>
 
